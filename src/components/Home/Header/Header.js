4362001dd50f3090b5807f13642496a6a1b383ad
samiulislam09 <samiulislamsawon09@gmail.com>
2022-05-05 00:11:29 +0600

    added navbar

---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    <Nav.Link as={Link} to='/' href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                    <Nav>
+                    <Nav.Link href="#deets">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </div>
+  )
+}
+
+export default Header
